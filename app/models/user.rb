67058777e125a221f49ffb6bd2b849ebcd1c8209
Sylvia <sylviapap1@gmail.com>
2020-03-05 10:24:13 -0800

    add self join to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,14 @@ class User < ApplicationRecord
     has_many :chats, foreign_key: :owner_id
     has_many :messages
     belongs_to :language
-    
+
+    # contacts = your contacts (followers)
+    # contacted = users who have you as contact (followed)
+
+    has_many :contacts, foreign_key: :contact_id, class_name: "Contact"
+    has_many :contacted, through: :contacts
+    has_many :contacted, foreign_key: :contacted_id, class_name: "Contact"
+    has_many :contacts, through: :contacted
 
 end
 
