b3694612cab2317e63f3c46fec1c2d0fdbcff947
Sylvia <sylviapap1@gmail.com>
2020-03-05 10:45:13 -0800

    migration

--- db/migrate/20200305181551_create_contacts.rb
+++ db/migrate/20200305181551_create_contacts.rb
@@ -1,6 +1,8 @@
 class CreateContacts < ActiveRecord::Migration[6.0]
   def change
     create_table :contacts do |t|
+      t.integer "contact_id"
+      t.integer "contacted_id"
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_05_034256) do
+ActiveRecord::Schema.define(version: 2020_03_05_181551) do
 
   create_table "chat_users", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 2020_03_05_034256) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.integer "contact_id"
+    t.integer "contacted_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "languages", force: :cascade do |t|
