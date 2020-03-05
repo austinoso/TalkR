4071bcf3d3c37089c76c9aad120b6763878ab032
Sylvia <sylviapap1@gmail.com>
2020-03-05 10:18:08 -0800

    contact migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,7 @@ class User < ApplicationRecord
     has_many :chats, foreign_key: :owner_id
     has_many :messages
     belongs_to :language
-
-    # has_many :user_languages
-    # has_many :languages, through: :user_languages
+    
 
 end
 
