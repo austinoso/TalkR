e0cdbfe497c847cef8e1d7876488ae69a2b40f6e
Sylvia <sylviapap1@gmail.com>
2020-03-06 11:38:11 -0800

    delete contact works

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,15 @@
 class ContactsController < ApplicationController
     def index
+        @contacts = current_user.contacts
+    end
+
+    def show
+        @contacts = current_user.contacts
+        @contact = @contacts.find(params[:id]) if @contacts.find(params[:id])
     end
 
     def destroy
+        current_user.contacts.delete(@user)
+        redirect_to contacts_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :messages, except: [:new, :edit, :update]
   resources :chats
   resources :users, except: [:new]
-  resources :contacts, only: [:index, :destroy]
+  resources :contacts, only: [:index, :show, :destroy]
 
   get '/login', to: 'sessions#new', as: 'login'
   get '/register', to: 'users#new', as: 'register'
