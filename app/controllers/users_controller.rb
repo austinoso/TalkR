7125f50861d8b938247cbefd575dc1cefa3a291a
Sylvia <sylviapap1@gmail.com>
2020-03-05 16:37:02 -0800

    fixed current_user show bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,7 +62,9 @@ class UsersController < ApplicationController
     end
 
     def current_user
-        @current_user = User.find(session[:user_id])
+        if session[:user_id]
+            @current_user = User.find(session[:user_id])
+        end
     end
 
     def user_authorize
